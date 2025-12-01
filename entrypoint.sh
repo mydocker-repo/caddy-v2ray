1bca0dd80147be383aa18054cede908565a71da5
mydocker-repo <45119302@qq.com>
2025-12-01 13:05:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 # entrypoint.sh
 
 caddy run -c /etc/caddy/Caddyfile
-
+echo "Caddy 已经运行。。。"
+sleep 5
 v2ray run -c /etc/v2ray/config.json
-
+echo "V2ray 已经运行。。。"
 # 执行传入的命令（通常是 crond -f）
 exec "$@"
